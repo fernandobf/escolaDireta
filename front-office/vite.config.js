c976991d7ed1ef9514d161b5f7c1f285660eb089
fernandobf <fernandobravofigueroa@gmail.com>
2025-05-30 12:30:34 +0100

    Task #22 - Ensure _redirects is included

--- front-office/vite.config.js
+++ front-office/vite.config.js
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   //base: '/login/qrCode',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.' // coloca direto na raiz do dist
+        }
+      ]
+    })
+  ],
   server: {
     port: 5173,
   },
