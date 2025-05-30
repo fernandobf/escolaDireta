7dd1fe98e442ff0d50dfccb904860b8bc1334d70
fernandobf <fernandobravofigueroa@gmail.com>
2025-05-30 12:01:23 +0100

    Task #16 - Fixes dirname

--- back-end/qrCode.js
+++ back-end/qrCode.js
@@ -7,6 +7,10 @@ import QRCode from 'qrcode';
 import fs from 'fs';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
